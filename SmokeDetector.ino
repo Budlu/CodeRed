d0fdb26c50a0deaa0f93d7092455d1c8a9e4d010
Budlu <48537618+Budlu@users.noreply.github.com>
2020-10-25 08:29:34 -0400

    Update SmokeDetector.ino

--- SmokeDetector.ino
+++ SmokeDetector.ino
@@ -1,16 +1,22 @@
+#include <Servo.h>
+Servo servo;
+
 bool active = false;
 char inactiveFlag = 'a';
 char activeFlag = 'b';
 
 int hoseMotor = 4;
+int doorMotor = 7;
 int sensorPin = A0;
 
 int motorSpinTime = 1000;
 int threshold = 600;
+int waitTime = 1000;
 
 void setup() {
   pinMode(hoseMotor, OUTPUT);
   pinMode(sensorPin, INPUT);
+  servo.attach(doorMotor);
 
   // Begins serial communications with computer
   Serial.begin(9600);
@@ -21,8 +27,9 @@ void setup() {
 void loop() {
   int onFire = analogRead(sensorPin);
 
-  if (onFire > 600 && !active)
+  if (onFire > threshold && !active)
   {
+    // Update active to prevent system from activating twice
     active = true;
     activate();
   }
@@ -31,9 +38,14 @@ void loop() {
     int availableBytes = Serial.availableForWrite();
     if (availableBytes >= 1)
     {
-      Serial.write("a");
+      Serial.write(inactiveFlag);
     }
   }
+ 
+/*
+  Spins servo motor to close garage door when data is received from python script
+  Python script was intended to send data when phone sends message
+  Not implemented
 
   if (Serial.read() != -1)
   {
@@ -41,22 +53,28 @@ void loop() {
     delay(motorSpinTime);
     servo.write(0);
   }
- 
-  delay(1000);
+*/
+  
+  delay(waitTime);
 }
 
 void activate()
 {
   int availableBytes = Serial.availableForWrite();
+  
+  // Checks if a byte can be written
   if (availableBytes >= 1)
   {
-    Serial.write("b");
+    Serial.write(activeFlag);
+    
+    // Hose motor also controls garage door
     digitalWrite(hoseMotor, HIGH);
     delay(motorSpinTime);
     digitalWrite(hoseMotor, LOW);
   }
   else
   {
+    // If data isn't written, reset active and try again
     active = false;
   }
 }
